72e9d3538a043575423334ff10d69352e0b1ec38
Fabio <fabiodevbh@gmail.com>
2021-05-13 17:32:41 -0300

    C007 - Alterando informações do DATABASES - Info 2

--- fusion/settings.py
+++ fusion/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'fusion',
         'USER': 'fabio',
-        'PASSWORD': '$13Agoravai',
+        'PASSWORD': '$13Devbh',
         'HOST': 'localhost',
         'PORT': '5432',
 
