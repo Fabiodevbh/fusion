537527a1140e85a8c37ca807eb438933e7ad5fee
Fabio <fabiodevbh@gmail.com>
2021-05-24 19:37:51 -0300

    C031 - Mudança importante em views, edição em contato e hero

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core.mail.message import EmailMessage
 
+
 class ContatoForm(forms.Form):
     nome = forms.CharField(label='Nome', max_length=100)
     email = forms.EmailField(label='E-mail', max_length=100)
--- core/views.py
+++ core/views.py
@@ -1,19 +1,34 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 from .models import Servico, Funcionario
-
+from .forms import ContatoForm
 
 """
-Dentre as várias opções de view que temos, o instrutor optou por usar o TemplateView. Isso porque para usarmos o
+Dentre as várias opções de view que temos, INICIALMENTE o instrutor optou por usar o TemplateView. Isso porque para usarmos o
 TemplateView, precisamos somente informar o nome do template na classe que criamos abaixo (class IndexView)
+
+No commit C031 o instrutor fez uma mudança, trocando o TemplateView por FormView...
 """
 
 
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name = 'index.html'
+    form_class = ContatoForm
+    success_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['servicos'] = Servico.objects.all()
         context['funcionarios'] = Funcionario.objects.all()
         return context
+
+    def form_valid(self, form, *args, **kwargs):
+        form.send_mail()
+        messages.success(self.request, 'E-mail enviado com sucesso!')
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Erro ao enviar e-mail')
+        return super(IndexView, self).form_invalid(form, *args, **kwargs)
