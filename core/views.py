8d119868523adececf53bf9efc655aca959be26d
Fabio <fabiodevbh@gmail.com>
2021-05-24 17:25:49 -0300

    C028 - Método get_context_data para diminuir/simplificar código

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
 
+from .models import Servico, Funcionario
+
 
 """
 Dentre as várias opções de view que temos, o instrutor optou por usar o TemplateView. Isso porque para usarmos o
@@ -10,3 +12,8 @@ TemplateView, precisamos somente informar o nome do template na classe que criam
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['servicos'] = Servico.objects.all()
+        context['funcionarios'] = Funcionario.objects.all()
+        return context
