272e0a6722bd57055c187255edec39fa6cc3d204
Fabio <fabiodevbh@gmail.com>
2021-05-31 10:23:15 -0300

    C039 - Debug False

--- fusion/settings.py
+++ fusion/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%+siy7nr)^g66f$2)i+ky(^h%++glh6hfx(v*tjlp^vbudt*@&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
